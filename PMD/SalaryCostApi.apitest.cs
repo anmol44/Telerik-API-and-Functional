ad9cec730d93ad854e85ccf1f1f511992c90acc0
CHAUDHARY Anmol <anmol.chaudhary@soprasteria.com>
2022-10-15 18:28:16 +0530

    Updating File type from Excel to csv

--- PMD/SalaryCostApi.apitest.cs
+++ PMD/SalaryCostApi.apitest.cs
@@ -4,6 +4,9 @@ using Telerik.ApiTesting.Framework.Abstractions;
 using APIAutomation.Utilities;
 using Newtonsoft.Json.Linq;
 using GemBox.Spreadsheet;
+using PMD.Utilities;
+using System.Globalization;
+using System.IO;
 
 namespace APIAutomation
 {
@@ -12,10 +15,20 @@ namespace APIAutomation
     {				
 	public void TestMethod()
 		{
-			String[] lastnames ={"26424", "30730","33793", "102891", "59260", "60393", "112142", "112174", "112396", "176447", "129877", "174711", "179106", "135231", "189384", "191829", "210490", "233334", "236829", "257356"};	
-		    int length = lastnames.GetLength(0);
-			this.Context.SetValue("length",length.ToString());
+			String[] lastnames ={"2642456", "30730","33793", "102891", "59260", "60393", "112142", "112174", "112396", "176447", "129877", "174711", "179106", "135231", "189384", "191829", "210490", "233334", "236829", "257356"};	
+		   // int length = lastnames.GetLength(0);
+			int loopTest = 3000;
+			int arrayLength = lastnames.GetLength(0);
+			this.Context.SetValue("length",loopTest.ToString());
+			int rowNumber = Int32.Parse(this.Context.GetValue("row").ToString());
+			rowNumber =rowNumber+1;
+			this.Context.SetValue("row",rowNumber.ToString());
+			
 			int loop = Int32.Parse(this.Context.GetValue("inc").ToString());
+			if(loop%arrayLength==0){
+				this.Context.SetValue("inc",0);
+				loop = Int32.Parse(this.Context.GetValue("inc").ToString());
+			}
 			String lm = lastnames[loop];
 			loop=loop+1;
 			this.Context.SetValue("inc",loop.ToString());
@@ -59,6 +72,7 @@ namespace APIAutomation
 		   }
 		
 	public void ResultToExcel(){	
+		// @Anmol Chaudhary
 			string code="";
 			string path ="";
 			string errors="";
@@ -69,12 +83,17 @@ namespace APIAutomation
 			string result ="";
 		    string env ="";
 		    string vers = "";
+		    string fileName = "";
 			
 			//path of file
 			path  =  this.Context.GetValue("path").ToString();
+		    
+		    // name of File
+		    fileName = this.Context.GetValue("fileName").ToString();
 			
 			// calculate row
-			row = this.Context.GetValue("inc").ToString();
+			//row = this.Context.GetValue("inc").ToString();
+	     	 row = this.Context.GetValue("row").ToString();
 			
 			//calculate id
 			id = this.Context.GetValue("id").ToString();
@@ -111,17 +130,18 @@ namespace APIAutomation
 			
 			try{				
 				foreach(JToken token in j.FindTokens("Code")){
-					code = code+token.ToString()+"\n";					
+					code = code+token.ToString()+Environment.NewLine;				
 				}				
 				foreach(JToken token in j.FindTokens("Message")){
-					errors = errors+token.ToString()+"\n";
+					errors = errors+token.ToString() + Environment.NewLine;
 				}								
 		        //	 code = j["Results"][0]["IdPerson"].ToString();				
 			 }catch(Exception e){
 				this.Log.WriteLine("no value for IdPerson");				
-			      }		
+			      }	
 			 
-			DataToExcel.createDataExcelFile(path,"salaryApi",row,id,result,responseTime,statusCode,code,errors,env,vers);			
+			//DataToExcel.createDataExcelFile(path,"salaryApi",row,id,result,responseTime,statusCode,code,errors,env,vers);		
+		   CreateCSV.addrecord(row,id,result,responseTime,statusCode,code,errors,env,vers,path.ToString()+fileName.ToString());
 			this.Log.WriteLine(errors);			
 		}
 		
@@ -136,13 +156,30 @@ namespace APIAutomation
 		
 		// This method will execute before the start of the test case
 		public override void OnBeforeTestStarted()
+			// @Anmol chaudhary
 			{
 			 string value="";
 			 string path="";
+			 string fileName="";
    			 this.Log.WriteLine("The test case is starting ...");
 			 this.Log.WriteLine("Create Excel Result File");
 			 path  =  this.Context.GetValue("path").ToString();	
-			CreateExcel.createExcel("salaryApi" ,path.ToString());
+			 // name of File
+		    fileName = this.Context.GetValue("fileName").ToString();
+		//	CreateExcel.createExcel("salaryApi" ,path.ToString());
+				
+		// File Handling.		
+				try{
+					if(File.Exists(path.ToString()+fileName.ToString())){
+						File.Delete(path.ToString()+fileName.ToString());
+					} else {
+						this.Log.WriteLine("File Not Find");
+					}
+				}catch(Exception e){
+					this.Log.WriteLine("Regarding file :: " +e.Message.ToString());
+				}
+			
+		    CreateCSV.addrecord("SERIAL NUMBER","ID","RESULT","RESPONSE TIME","STATUS CODE","ERROR CODE","ERROR MESSAGE","ENVIRONMENT","VERSION",path.ToString()+fileName.ToString());
 			}
 
 // This method will execute after all test steps in the test case have ended
